9a8eda6bb5a0eb97c2bdb1696e93cab6ee953011
hemankur <hemankur@hotmail.com>
2019-11-24 17:01:01 -0500

    Return access token and refresh token

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,5 +1,8 @@
 import express = require('express');
-import session = require('express-session');
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 
 import bcrypt = require('bcrypt');
 import mysql = require('mysql');
@@ -22,8 +25,7 @@ app.use(bodyParser.json());
 let cors = require('cors');
 app.use(cors());
 
-app.get('/api/users/', (req, res) => {
-
+app.get('/api/users/', authenticateToken, (req, res) => {
 
     connection.query('SELECT * FROM users', (err, rows) => {
         if (err) {
@@ -55,9 +57,6 @@ app.post('/api/user/create/', async (req, res) => {
     let salt = await bcrypt.genSalt();
     let hashedPassword = await bcrypt.hash(plaintextPassword, salt);
 
-    console.log(req.body.username);
-    console.log(salt);
-    console.log(hashedPassword);
 
     let sql = 'insert into users (username, password) values (?, ?)';
     let params = [req.body.username, hashedPassword];
@@ -86,24 +85,69 @@ app.post('/api/user/login/', async (req, res) => {
 
     let sql = 'select * from users where username = ?';
     let params = [username];
+    let errorMessage = 'Invalid Username or Password';
 
     connection.query(sql, params, async (err, rows) => {
         if (err) {
             console.log(err);
         } else {
-            if (rows[0].username.toLowerCase() === username.toLowerCase()) {
-                bcrypt.compare(password, rows[0].password, function (err, res) {
+            if (rows.length === 0) {
+                res.json({
+                    error: errorMessage
+                });
+                return;
+            } else if (rows[0].username.toLowerCase() === username.toLowerCase()) {
+                bcrypt.compare(password, rows[0].password, function (err, result) {
                     if (err) {
                         console.log(err);
                     }
-                    console.log(res);
+                    if (!result) {
+                        res.json({
+                            error: errorMessage
+                        });
+                        return;
+                    } else {
+                        const user = {name: rows[0].username};
+                        const accessToken = generateAccessToken(user);
+                        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'});
+                        let sql = 'update users set token = ? where username = ? ';
+                        let params = [refreshToken, user.name];
+                        connection.query(sql, params, (err, rows) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log(rows);
+                            }
+                        });
+                        res.json({
+                            message: 'success',
+                            accessToken: accessToken,
+                            refreshToken: refreshToken
+                        });
+                    }
                 });
             }
         }
     });
 
     if (errors.length) {
-        res.status(400).json({error: 'invalid username or password'});
+        res.status(400).json({error: errors});
         return;
     }
 });
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token === null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next()
+    });
+}
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1m'});
+}
